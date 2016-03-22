87514435ad289bf73c6e161b202917cbdcd7002a
Ruben Jenster <r.jenster@drachenfels.de>
2016-03-22 16:36:27 +0100

    Added simple test case. Updated copyright notice.

--- command_test.go
+++ command_test.go
@@ -1,4 +1,4 @@
-// Copyright 2013 Google Inc. All Rights Reserved.
+// Copyright 2016 Drachenfels GmbH. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,165 +15,39 @@
 package command
 
 import (
-	"flag"
-	"os"
 	"testing"
 )
 
-// Tests if global flags default values are set if there are
-// no flags provided.
-func TestDefaultGlobalFlags(t *testing.T) {
-	resetForTesting()
+func TestMissingCommand(t *testing.T) {
+	t.Skip("Not implemented.")
+}
 
-	flagGlobal1 := flag.String("global1", "default-global1", "Description about global1")
-	Parse()
-	if *flagGlobal1 != "default-global1" {
-		t.Error("global flag should be set to default val if the flag is not set")
+func TestMissingParams(t *testing.T) {
+	t.Skip("Not implemented.")
+}
+
+func TestInvalidParams(t *testing.T) {
+	t.Skip("Not implemented.")
+}
+
+func TestCommandFunc(t *testing.T) {
+	var val string
+	On(
+		"hello",
+		"simple testing function",
+		CmdFunc(func(args []string) error {
+			val = args[0]
+			return nil
+
+		}),
+		[]string{},
+	)
+
+	err := Run("hello", "world")
+	if err != nil {
+		t.Fatalf(err.Error())
 	}
-}
-
-// Tests if global flags are set if they are provided by the user.
-func TestGlobalFlags(t *testing.T) {
-	resetForTesting("-global1=hello")
-
-	flagGlobal1 := flag.String("global1", "default-global1", "Description about global1")
-	Parse()
-	if *flagGlobal1 != "hello" {
-		t.Errorf("global flag should be set: expected default-global1, found %s", *flagGlobal1)
+	if val != "world" {
+		t.Fatalf("Command should set val to %q but was %q.", "world", val)
 	}
-}
-
-// Tests the total number of globally registered flags.
-func TestGlobalFlagsCount(t *testing.T) {
-	resetForTesting("-global1=hello", "-global2=hi")
-
-	flag.String("global1", "default-global1", "Description about global1")
-	flag.String("global2", "default-global2", "Description about global2")
-	Parse()
-
-	total := numOfGlobalFlags()
-	if total != 2 {
-		t.Error("total number of global flags are expected to be 2, found %v", total)
-	}
-}
-
-// Tests if subcommand runs if it's provided as a part of arguments.
-func TestCommand(t *testing.T) {
-	resetForTesting("-global1=hello", "command1")
-
-	flagGlobal1 := flag.String("global1", "default-global1", "Description about global1")
-	c1 := &testCmd1{}
-	On("command1", "", c1, []string{})
-	Parse()
-	Run()
-	if !c1.run {
-		t.Error("command 'command1' was expected to run, but it didn't")
-	}
-	if *c1.flag1 {
-		t.Errorf("flag1 should be set to default: expected false, found %v", *c1.flag1)
-	}
-	if *flagGlobal1 != "hello" {
-		t.Errorf("global flag should be set: expected default-global1, found %s", *flagGlobal1)
-	}
-}
-
-// Tests if subcommand runs and subcommand flags are set.
-func TestCommandFlags(t *testing.T) {
-	resetForTesting("-global1=hello", "command1", "-flag1=true")
-
-	flag.String("global1", "default-global1", "Description about global1")
-	c1 := &testCmd1{}
-	On("command1", "", c1, []string{})
-	Parse()
-	Run()
-	if !c1.run {
-		t.Error("command 'command1' was expected to run, but it didn't")
-	}
-	if !*c1.flag1 {
-		t.Errorf("flag1 should be set: expected true, found %v", *c1.flag1)
-	}
-}
-
-// Tests if correct subcommand runs if multiple subcommands
-// are registered.
-func TestMultiCommands(t *testing.T) {
-	resetForTesting("command2")
-
-	c1 := &testCmd1{}
-	c2 := &testCmd2{}
-	On("command1", "", c1, []string{})
-	On("command2", "", c2, []string{})
-	Parse()
-	Run()
-	if c1.run {
-		t.Error("command 'command1' was not expected to run, but it did")
-	}
-	if !c2.run {
-		t.Error("command 'command2' was expected to run, but it didn't")
-	}
-}
-
-// Tests if subcommand runnable has run, if Run is not invoked.
-func TestRun(t *testing.T) {
-	resetForTesting("command1")
-
-	c1 := &testCmd1{}
-	On("command1", "", c1, []string{})
-	Parse()
-	if c1.run {
-		t.Error("command 'command1' was not expected to run, but it did")
-	}
-}
-
-func TestAdditionalCommandArgs(t *testing.T) {
-	resetForTesting("command1", "--flag1=true", "somearg")
-
-	c1 := &testCmd1{}
-	On("command1", "", c1, []string{})
-	Parse()
-	if len(args) < 1 || args[0] != "somearg" {
-		t.Error("additional command 'somearg' is expected, but can't be found")
-	}
-}
-
-// Resets os.Args and the default flag set.
-func resetForTesting(args ...string) {
-	os.Args = append([]string{"cmd"}, args...)
-	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
-}
-
-// testCmd1 is a test sub command.
-type testCmd1 struct {
-	flag1 *bool
-
-	run bool
-}
-
-// Defines flags for the sub command.
-func (cmd *testCmd1) Flags(fs *flag.FlagSet) {
-	cmd.flag1 = fs.Bool("flag1", false, "Description about flag1")
-}
-
-// Sets the run flag.
-func (cmd *testCmd1) Run(args []string) error {
-	cmd.run = true
-	return nil
-}
-
-// testCmd2 is a test sub command.
-type testCmd2 struct {
-	flag2 *bool
-
-	run bool
-}
-
-// Defines flags for the sub command.
-func (cmd *testCmd2) Flags(fs *flag.FlagSet) {
-	cmd.flag2 = fs.Bool("flag2", false, "Description about flag2")
-}
-
-// Sets the run flag.
-func (cmd *testCmd2) Run(args []string) error {
-	cmd.run = true
-	return nil
 }
